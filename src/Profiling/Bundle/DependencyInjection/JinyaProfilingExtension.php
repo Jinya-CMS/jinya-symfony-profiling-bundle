1834c41835a37569dcb936616ad33f82ca01f7d5
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-11-12 21:55:15 +0100

    Added loading of default settings

--- src/Profiling/Bundle/DependencyInjection/JinyaProfilingExtension.php
+++ src/Profiling/Bundle/DependencyInjection/JinyaProfilingExtension.php
@@ -9,8 +9,10 @@
 namespace Jinya\Profiling\Bundle\DependencyInjection;
 
 use Jinya\Profiling\Bundle\EventSubscriber\Profiling\JinyaProfilerEventSubscriber;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class JinyaProfilingExtension extends Extension
 {
@@ -20,9 +22,13 @@ class JinyaProfilingExtension extends Extension
      *
      * @param array $configs
      * @param ContainerBuilder $container
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader->load('services.yml');
+
         $configuration = new Configuration();
 
         $config = $this->processConfiguration($configuration, $configs);
