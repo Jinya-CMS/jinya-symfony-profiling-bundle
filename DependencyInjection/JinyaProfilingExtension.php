231c1274f94e307a46f256909b7dc9051f1adcaf
Imanuel Ulbricht <imanuel@ulbricht.codes>
2019-01-02 05:06:45 +0100

    Fixed service configuration

--- DependencyInjection/JinyaProfilingExtension.php
+++ DependencyInjection/JinyaProfilingExtension.php
@@ -8,6 +8,7 @@
 
 namespace JinyaProfiling\Bundle\DependencyInjection;
 
+use JinyaProfiling\Bundle\EventSubscriber\JinyaProfilerEventSubscriber;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -28,8 +29,8 @@ class JinyaProfilingExtension extends ConfigurableExtension
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
         $loader->load('services.yml');
 
-        $definition = $container->getDefinition('jinya_profiling.event_subscriber');
-        $definition->setArgument('profilerOutDir', $configs['out_dir']);
-        $definition->setArgument('profilerEnabled', $configs['enabled']);
+        $definition = $container->getDefinition(JinyaProfilerEventSubscriber::class);
+        $definition->replaceArgument('$profilerOutDir', $configs['out_dir']);
+        $definition->replaceArgument('$profilerEnabled', $configs['enabled']);
     }
 }
